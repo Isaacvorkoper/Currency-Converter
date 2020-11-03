a8c32b4704e1cd8cc70adde57e30e1829bdece3c
Isaac Vorköper <isaaclennart@gmail.com>
2020-11-03 11:24:02 +0100

    Design finished.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,41 @@ namespace Valutaomregner
         public MainWindow()
         {
             InitializeComponent();
+            CurBox1.Items.Add("USD");
+            CurBox1.Items.Add("EUR");
+            CurBox1.Items.Add("GBP");
+            CurBox1.Items.Add("DKK");
+            CurBox1.Items.Add("SEK");
+            CurBox1.Items.Add("NOK");
+
+            CurBox2.Items.Add("USD");
+            CurBox2.Items.Add("EUR");
+            CurBox2.Items.Add("GBP");
+            CurBox2.Items.Add("DKK");
+            CurBox2.Items.Add("SEK");
+            CurBox2.Items.Add("NOK");
+        }
+
+        private void ConBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox.Text == "Indtast beløb")
+                txtBox.Text = string.Empty;
+        }
+
+        private void CurBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CurBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ResBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
